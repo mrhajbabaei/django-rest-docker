4820a824e6448393dd8b872362ff460965834356
mrhajbabaei <hajbabaei.mr@gmail.com>
2020-12-19 20:44:19 +0330

    add modles and migrations

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -14,7 +13,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser, PermissionMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
@@ -23,4 +22,4 @@ class User(AbstractBaseUser, PermissionMixin):
 
     objects = UserManager()
 
-    USERNAMAE_FIELD = 'email'
+    USERNAME_FIELD = 'email'
