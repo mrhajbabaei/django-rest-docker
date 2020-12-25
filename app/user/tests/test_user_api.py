62cf0974da6a529feef497a45fd5c272cc58512b
mrhajbabaei <hajbabaei.mr@gmail.com>
2020-12-25 12:16:30 +0330

    added urls & views for user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -23,17 +23,17 @@ class PublicTest(TestCase):
         """Test creating user with valid payload is successful"""
         payload = {
             'email': 'test@gmail.com',
-            'passworkd': 'testpass',
+            'password': 'testpass',
             'name': 'Test name'
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.asserEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(**res.data)
-        self.asserTrue(user.check_password(payload['password']))
+        self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
-    def test_user_exists(sefl):
+    def test_user_exists(self):
         """Test creating a user that already exists fails"""
         payload = {'email': 'test@gmail.com', 'password': 'testpass'}
         create_user(**payload)
@@ -47,7 +47,7 @@ class PublicTest(TestCase):
         payload = {'email': 'test@gmail.com', 'password': 'pw'}
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
