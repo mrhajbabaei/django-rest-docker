1e13f407de85a16fd8e3fc9c9c2ad51c22de8ee0
mrhajbabaei <hajbabaei.mr@gmail.com>
2020-12-26 22:41:07 +0330

    added tag authorized and unauthorized tests

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from recipe import serializers
+from core.models import Tag
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+form rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage tags in the database"""
+    authentication_classses = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
